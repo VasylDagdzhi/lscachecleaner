83cfa5dad1dbb0ecdb8a4a75ff09fba9e493adac
LJNERO8 <ljnero8@gmail.com>
2022-12-11 06:48:57 +0200

    basic functionality

---
+++ clear_users_lscache.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+################################################################################
+#
+# Collects user's old cache files which are older than 24h inside:
+# /home/user/lscache directories and removes them to free up storage space
+# in case there is not enough storage left in /home partition 
+# 
+# Author:  Vasiliy Dagdzhi
+# e-mail:  vasiliy.dagdzhi@namecheap.com
+# Date:    2022-12-11
+# Version: 1.0.0
+#
+################################################################################
+echo "Script name is: $0"
+echo "Arg1 is $1"
+echo "Arg1 is $2"
+echo "Arg1 is $3"
+echo "-----------"
+echo "All args: $*"
+echo "All args count: $#"
+
+# collect all user's lscache folders which have at least over 1Mb or 1Gb of space inside:
+large_folders_list=$(find /home -maxdepth 2 -name "lscache" -exec du -shx {} \;| grep -E "M|G" | awk '{print $2}')
+
+
+# Print the number of such users are on the server
+echo -e "\033[0;32mFound: $(echo -e "$large_folders_list" | wc -l)" "\t user's with large lscache folders.\n"
+
+# list the users who own the large folders and print them
+echo -e "\033[0;32m\nUser's list:"
+
+for i in $large_folders_list;
+do
+  	awk -F/ '{print $3}' <<< "$i"
+done;
+
+# locate all files inside these folders which are older than 24h and remove them
+# excluding: .cm.log files from the list
+
+totalSize=0
+
+for i in $large_folders_list;
+do
+	# calculate the size of files which are going to be removed
+	allSizes=$(find "$i" ! -iname ".cm.log" -type f -mtime +1 -exec stat -c "%s" {} \;)
+	for fileSize in $allSizes; do
+    		totalSize=$(($totalSize + $fileSize))
+	done
+	
+	# perform removal
+  	find "$i" ! -iname ".cm.log" -type f  -mtime +1 -exec ls -lht {} \; # -exec rm -f {} \;
+done;
+
+
+echo  -e "$($totalSize/1024/1024/1024) GB - removed."
